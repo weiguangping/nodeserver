22bb1be77392a5b71eae27852475cf97a6743b0d
“tuimeo_weiguangping <weiguangping@tuimeo.cn>
2018-01-12 17:58:03 +0800

    1

---
+++ routes/good.js
@@ -0,0 +1,61 @@
+var express = require('express')
+var router = express.Router()
+var mongoose = require('mongoose')
+var Goods = require('../models/goods')
+
+// 连接MongoDB数据库
+mongoose.connect('mongodb://127.0.0.1:27017/imoocmall')
+mongoose.connection.on('connected', function () {
+  console.log('MongoDB connected success.')
+})
+
+mongoose.connection.on('error', function () {
+  console.log('MongoDB connected fail.')
+})
+mongoose.connection.on('disconnected', function () {
+  console.log('MongoDB connected disconnected.')
+})
+// 查询商品列表数据
+router.get('/list', function (req, res, next) {
+  let page = parseInt(req.param('page'))
+  let pageSize = parseInt(req.param('pageSize'))
+  let priceLevel = req.param('priceLevel')
+  let sort = req.param('sort')
+  let skip = (page - 1) * pageSize
+  var priceGt = '', priceLte = ''
+  let params = {}
+  if (priceLevel != 'all') {
+    switch (priceLevel) {
+      case '0': priceGt = 0; priceLte = 100; break
+      case '1': priceGt = 100; priceLte = 500; break
+      case '2': priceGt = 500; priceLte = 1000; break
+      case '3': priceGt = 1000; priceLte = 5000; break
+    }
+    params = {
+      salePrice: {
+        $gt: priceGt,
+        $lte: priceLte
+      }
+    }
+  }
+  let goodsModel = Goods.find(params).skip(skip).limit(pageSize)
+  goodsModel.sort({ 'salePrice': sort })
+  goodsModel.exec(function (err, doc) {
+    if (err) {
+      res.json({
+        status: '1',
+        msg: err.message
+      })
+    } else {
+      res.json({
+        status: '0',
+        msg: '',
+        result: {
+          count: doc.length,
+          list: doc
+        }
+      })
+    }
+  })
+})
+module.exports = router
---
+++ routes/users.js
@@ -0,0 +1,42 @@
+var express = require('express')
+var router = express.Router()
+var User = require('./../models/user')
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+  res.render('users', { title: 'Express' })
+})
+router.get('/test', function (req, res, next) {
+  res.send('test')
+})
+router.post('/login', function (req, res, next) {
+  var param = {
+    userName: req.body.userName,
+    userPwd: req.body.userPwd
+  }
+  User.findOne(param, function (err, doc) {
+    if (err) {
+      res.json({
+        status: '1',
+        msg: err.message
+      })
+    } else {
+      if (doc) {
+        res.cookie('userId', doc.userId, {
+          path: '/',
+          maxAge: 1000 * 60 * 60
+        })
+        req.session.user = doc
+        res.json({
+          status: '0',
+          msg: '',
+          result: {
+            userName: doc.userName
+          }
+        })
+      }
+    }
+  })
+  res.send('test')
+})
+
+module.exports = router
